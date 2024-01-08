1a0e5a8845c7a5c84838352448aff50d900179ae
zubairmoerat <zubairmoeratofficial@gmail.com>
2024-01-08 13:59:16 +0200

    footer and jsonfile Done

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,16 @@
   <main class="container-fluid">
     <router-view/>
   </main>
+  <Footer></Footer>
 </template>
 
 <script>
 import NavBar from './components/NavBar.vue';
-
+import Footer from './components/Footer.vue';
 export default{
   components: {
     NavBar,
+    Footer
 }
 }
 </script>
