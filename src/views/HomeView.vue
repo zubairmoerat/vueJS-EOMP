7460a835b2917ec564c8ef691095457aeb4c850a
zubairmoerat <zubairmoeratofficial@gmail.com>
2024-01-08 15:52:40 +0200

    almostDone

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,37 @@
 <template>
-  <div>
-    <h1>this is my homepage</h1>
+  <div class="container-fluid">
+    <div class="row">
+      <h1>Welcome</h1>
+    </div>
+    <div class="row d-flex">
+      <div class="col" v-if="jobTitle">
+        <h3 v-for="title in jobTitle" :key="title.id">{{ title }}</h3>
+      </div>
+      <div class="col">
+        <img class="img-fluid" src="https://drive.google.com/uc?id=1G3WJXBiXyM8o8wr1XLjRBQA2IIsjvZBr" alt="portfolio" loading="lazy">
+      </div>
+    </div>
+    <div class="row">
+      <Spinner></Spinner>
+    </div>
   </div>
 </template>
 
 <script>
+import Spinner from '@/components/Spinner.vue';
 
 export default {
   name: 'HomeView',
   components: {
+    Spinner
+  },
+  computed: {
+    jobTitle() {
+      return this.$store.state.jobTitle
+    }
+  },
+  mounted() {
+    this.$store.dispatch('fetchJobTitle')
   }
 }
 </script>
